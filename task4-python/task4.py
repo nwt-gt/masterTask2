5aeca98c3534ad82918731276947f7c4e286cb2b
nwt <weetong@dsaid.gov.sg>
2020-05-17 12:42:14 +0000

    changed task4

--- task4-python/task4.py
+++ task4-python/task4.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sys
 
+#check that user input 1 csv file
 try:
     if len(sys.argv) < 1:
         print("Missing files. A csv file must be passed")
@@ -11,10 +12,14 @@ filename = sys.argv[1]
 
 data = pd.read_csv(filename)
 #data = pd.read_csv(r'C:\Users\weetong\Desktop\DE\dataeng_test-master\04-17-2020_Dataset.csv')
-dataExtracted = pd.DataFrame(data, columns= ['price','firstName'])
-jsonFormat = dataExtracted.to_json(orient ='records')
+dataExtracted = pd.DataFrame(data, columns= ['price','firstName']) #extrac 3rd and 1st column
+jsonFormat = dataExtracted.to_json(orient ='records') #change to json format
 
-print(dataExtracted.head())
+#print(jsonFormat)
+#print(dataExtracted.head())
+
+
+###function to output xml###
 
 def func(row):
     xml = ['<item>']
@@ -25,3 +30,5 @@ def func(row):
     return '\n'.join(xml)
 
 #print('\n'.join(dataExtracted.apply(func, axis=1)))
+
+print("Uncomment to see output")
