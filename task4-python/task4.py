2eca35761644926896b27d9305b3d61c9025bc1f
nwt <weetong@dsaid.gov.sg>
2020-05-17 13:16:55 +0000

    csv

--- task4-python/task4.py
+++ task4-python/task4.py
@@ -1,34 +1,35 @@
 import pandas as pd
 import sys
 
-#check that user input 1 csv file
-try:
-    if len(sys.argv) < 1:
-        print("Missing files. A csv file must be passed")
-except IndexError as err:
-    print("HI")
-    
-filename = sys.argv[1]
+if __name__ == "__main__":
+    #check that user input 1 csv file
+    try:
+        if len(sys.argv) !=1:
+            raise ValueError("Please include 1 csv file")
+        else:
+            filename = sys.argv[1]
 
-data = pd.read_csv(filename)
-#data = pd.read_csv(r'C:\Users\weetong\Desktop\DE\dataeng_test-master\04-17-2020_Dataset.csv')
-dataExtracted = pd.DataFrame(data, columns= ['price','firstName']) #extrac 3rd and 1st column
-jsonFormat = dataExtracted.to_json(orient ='records') #change to json format
+            data = pd.read_csv(filename)
+            #data = pd.read_csv(r'C:\Users\weetong\Desktop\DE\dataeng_test-master\04-17-2020_Dataset.csv')
+            dataExtracted = pd.DataFrame(data, columns= ['price','firstName']) #extrac 3rd and 1st column
+            jsonFormat = dataExtracted.to_json(orient ='records') #change to json format
 
-#print(jsonFormat)
-#print(dataExtracted.head())
+            #print(jsonFormat)
+            #print(dataExtracted.head())
 
 
-###function to output xml###
+            ###function to output xml###
 
-def func(row):
-    xml = ['<item>']
-    for field in row.index:
-        xml.append('  <field name="{0}">{1}</field>'.format(field, row[field]))
-    xml.append('</item>')
-    
-    return '\n'.join(xml)
+            def func(row):
+                xml = ['<item>']
+                for field in row.index:
+                    xml.append('  <field name="{0}">{1}</field>'.format(field, row[field]))
+                xml.append('</item>')
+                
+                return '\n'.join(xml)
 
-#print('\n'.join(dataExtracted.apply(func, axis=1)))
+            #print('\n'.join(dataExtracted.apply(func, axis=1)))
 
-print("Uncomment to see output")
+            print("Uncomment to see output")
+    except:
+        print("Invalid argument.Please` Include 1 csv file")
