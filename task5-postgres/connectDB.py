d11c87ab25e509c44274055d3e0d8cc187dff49c
nwt <weetong@dsaid.gov.sg>
2020-05-17 16:35:38 +0000

    added connectiondb

--- task5-postgres/connectDB.py
+++ task5-postgres/connectDB.py
@@ -1,12 +1,40 @@
 import psycopg2
-conn = psycopg2.connect("host='172.20.0.2' port='5432' dbname='testdb' user='weetong' password='postgres'")
-cur = conn.cursor()
 
-cur.execute("""CREATE TABLE logs(
-        firstname text,
-        lastname text,
-        price text,
-        spending text
-)
-""")
-conn.commit()
+try:
+   connection = psycopg2.connect(user="postgres",
+                                  password="postgres",
+                                  host="127.0.0.1",
+                                  port="5450",
+                                  database="postgres")
+   cursor = connection.cursor()
+   postgreSQL_select_Query = """SELECT actor.actor_id, actor.first_name, actor.last_name, 
+                film.title, film.description, film.release_year
+                From 
+                film INNER JOIN film_actor ON film_actor.film_id = film.film_id
+                INNER JOIN actor ON film_actor.actor_id = actor.actor_id
+                limit 10
+"""
+
+   cursor.execute(postgreSQL_select_Query)
+   print("Selecting 10 rows using cursor.fetchmany")
+   #records = cursor.fetchall() 
+   records = cursor.fetchmany(10) 
+   
+   print("Print each row and it's columns values")
+   for row in records:
+       print("ACTOR_ID:   ", row[0])
+       print("FIRST_NAME:   ", row[1])
+       print("LAST_NAME:    ", row[2])
+       print("TITLE:   ", row[3])
+       print("DESCRIPTION:   ", row[4])
+       print("RELEASE_YEAR:   ", row[5], "\n")
+       print("")
+except (Exception, psycopg2.Error) as error :
+    print ("Error while fetching data from PostgreSQL", error)
+
+finally:
+    #closing database connection.
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
